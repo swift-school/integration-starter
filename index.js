6d0bc3ca829316c552516c065cbd3fa7ab3fda9b
Ben Dvorak <ben@augustschools.com>
2021-09-28 13:51:17 -0400

    fix root url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import _ from "lodash";
 import fetch from "node-fetch";
 
-const ROOT_URL = "https://august-schools.herokuapp.com";
+const ROOT_URL = "https://august-interview.herokuapp.com/";
 
 console.log("hello world!")
